b34ebaa35c8eba8e692c8e3267a89a5ff18372af
tahaghazy <68954755+tahaghazy@users.noreply.github.com>
2023-09-23 23:20:26 +0300

    Update header

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -3,6 +3,5 @@
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">about</NuxtLink></li>
         <li><NuxtLink to="/contact">contact</NuxtLink></li>
-
     </ul>
 </template>
